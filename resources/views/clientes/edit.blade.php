7aebdb2724e749579c5fd332f9d1ae42527052bc
Aderson Silva <aderson.batista19@gmail.com>
2025-09-08 18:59:19 -0300

    feature: add update and edit

---
+++ resources/views/clientes/edit.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+    <x-slot name="header">
+        {{ __('Home') }}
+    </x-slot>
+
+    <x-panel class="">
+
+        <x-splade-form :default="$cliente" :action="route('clientes.update', $cliente)" method="PUT">
+            <x-splade-input name="nome" label="Nome" />
+            <x-splade-input name="email" type="email" label="E-mail" />
+            <x-splade-input name="telefone" label="Telefone" />
+
+            <x-splade-submit label="Atualizar Cliente" />
+        </x-splade-form>
+    </x-panel>
+
+
+</x-layout>
